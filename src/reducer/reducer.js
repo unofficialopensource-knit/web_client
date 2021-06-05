92f270d67991b072485e05b61a9f43ccd0757803
pandeysambhi <pandeysambhi@gmail.com>
2021-06-05 15:54:40 +0530

    minor fixes

--- src/reducer/reducer.js
+++ src/reducer/reducer.js
@@ -7,6 +7,9 @@ const initialState={
 const reducer=(state={initialState},action)=>{
 
     switch(action.type){
+
+        case "action": return state;
+       
         default: return state;
     }
 
