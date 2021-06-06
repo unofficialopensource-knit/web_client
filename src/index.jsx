5244f8a7e170446f03d48a0d634ee1f30bf37b23
pandeysambhi <pandeysambhi@gmail.com>
2021-06-06 11:28:57 +0530

    anti-pattern fixes

--- src/index.jsx
+++ src/index.jsx
@@ -1,10 +1,8 @@
-import './index.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
-
+import './index.css';
+import App from './App';
 import React from 'react';
 import ReactDOM from 'react-dom';
-
-import App from './App';
 import reportWebVitals from './reportWebVitals';
 
 
