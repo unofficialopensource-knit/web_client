53cd54358cff1929c3c32ac11babb92bce052762
Mayank Pathak <onlinejudge95@gmail.com>
2021-07-05 13:43:52 +0530

    Fixes prettier check

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,15 @@
 import './App.css';
 import { Provider } from 'react-redux';
 import React from 'react';
-import Layout from './web-components/layout'
+import Layout from './web-components/layout';
 import store from './store/store';
-
 
 function App() {
   return (
     <Provider store={store}>
       <div className="App">
         <Layout />
-       </div>
+      </div>
     </Provider>
   );
 }
--- src/web-components/particleeffect.jsx
+++ src/web-components/particleeffect.jsx
@@ -1,35 +1,29 @@
-import Particles from 'react-particles-js'
-
+import Particles from 'react-particles-js';
 
 import React from 'react';
 
-const particleeffect = ({height,val}) => (
-	
-       	
-        <Particles
-          params={{
-	     "particles": {
-	        "number": {
-	            "value":val
-	        },
-	        "size": {
-	            "value": 3
-	        }
-	    },
-	    "interactivity": {
-	        "events": {
-	            "onhover": {
-	                "enable": true,
-	                "mode": "repulse"
-	            }
-	        }
-	    }
-	}}
-	height={height}
- />
-
-
-    );
+const particleeffect = ({ height, val }) => (
+  <Particles
+    params={{
+      particles: {
+        number: {
+          value: val,
+        },
+        size: {
+          value: 3,
+        },
+      },
+      interactivity: {
+        events: {
+          onhover: {
+            enable: true,
+            mode: 'repulse',
+          },
+        },
+      },
+    }}
+    height={height}
+  />
+);
 
 export default particleeffect;
-
